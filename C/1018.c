21f5ea9cbec4d1ba9b431b55e59a1637faeee68b
nullorm <null0rm@NoteBook.localdomain>
2023-09-15 12:32:11 +0900

    1018

--- C/1018.c
+++ C/1018.c
@@ -1,27 +1,64 @@
+// 백준 1018번 체크판 다시 칠하기
+// Rank : silver 4
+// 근데 좀더 어려운 것 같은데.. .
+
 #include <stdio.h>
 
 int main(void)
 {
     int M, N;
-    int B_count = 32, W_count = 32;
-    int res = 0;
+    char board[51][51] = {};
+    int tmpBoard[51][51] = {};
+    int tmp_min, min = 64;
+    int one_cnt, zero_cnt;
+    int x, y;
+
     scanf("%d %d", &M, &N);
-
-    char board[50][50] = {};
-
+    for (int i = 0; i < M; i++)
+        scanf("%s", board[i]);
+    // 첫 번쨰 값을 토대로 전체 board를 체스판 형태로 만든다. 바꾼 부분에다가는 1 표시.
     for (int i = 0; i < M; i++)
     {
+        for (int j = 0; j < N - 1; j++)
+        {
+            if (board[i][j] == board[i][j + 1])
+            {
+                tmpBoard[i][j + 1] = 1;
+                if (board[i][j] == 'W')
+                    board[i][j + 1] = 'B';
+                else if (board[i][j] == 'B')
+                    board[i][j + 1] = 'W';
+            }
         }
-    if (B_count > 0)
-    {
-        res += B_count;
+        if (board[i][0] == board[i + 1][0])
+        {
+            tmpBoard[i + 1][0] = 1;
+            if (board[i][0] == 'W')
+                board[i + 1][0] = 'B';
+            else if (board[i][0] == 'B')
+                board[i + 1][0] = 'W';
+        }
     }
-    if (W_count > 0)
+    // 최솟값을 찾아보도록 합시다 ...!
+    for (int i = 0; i < M - 7; i++)
     {
-        res += W_count;
+        for (int j = 0; j < N - 7; j++)
+        {
+            one_cnt = 0;
+            zero_cnt = 0;
+            for (x = 0; x < 8; x++)
+            {
+                for (y = 0; y < 8; y++)
+                {
+                    if (tmpBoard[i + x][j + y] == 1)
+                        one_cnt++;
+                    else
+                        zero_cnt++;
+                }
+            }
+            tmp_min = one_cnt > zero_cnt ? zero_cnt : one_cnt;
+            min = min < tmp_min ? min : tmp_min;
+        }
     }
-
-    printf("%d", res);
-    // 32개의 보드로 구성.
-    return 0;
+    printf("%d", min);
 }
